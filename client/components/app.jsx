d0f0f8a885365aab0ef09ccc5cdfefa09dae46fa
Steve Min <stevekmin@gmail.com>
2019-07-10 21:48:40 +0000

    define getProducts in App component retrieve all products with GET request

--- client/components/app.jsx
+++ client/components/app.jsx
@@ -8,6 +8,13 @@ export default class App extends React.Component {
       products: []
     };
   }
+  getProducts() {
+    fetch('/api/products.php')
+      .then(response => response.json())
+      .then(retrieveProducts => this.setState({
+        products: this.state.products(retrieveProducts)
+      }));
+  }
   render() {
     return (
       <Header />
