81e3f44fc357e92736fb0aed4c8ab604da0bd28a
Steve Min <stevekmin@gmail.com>
2019-07-11 23:34:25 +0000

    add two properties to App initial state name:'catalog' params:{}

--- client/components/app.jsx
+++ client/components/app.jsx
@@ -7,7 +7,9 @@ export default class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      products: []
+      products: [],
+      name: 'catalog',
+      params: {}
     };
   }
   componentDidMount() {
