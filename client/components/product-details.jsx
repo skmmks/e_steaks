d4b50fea6f5a27e68463bb62651d0c1fe79aa32d
Steve Min <stevekmin@gmail.com>
2019-07-11 22:39:00 +0000

    add componenetDidMount() to make call to api

--- client/components/product-details.jsx
+++ client/components/product-details.jsx
@@ -1,10 +1,21 @@
 import React from 'react';
-class ProductDetails extends React.Compontent {
+
+class ProductDetails extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
       product: null
     };
+  }
+  componentDidMount() {
+    fetch('/api/products/php?id=1')
+      .then(res => res.json())
+      .then(res => {
+        this.setState({
+          product: res
+        });
+      }
+      );
   }
   render() {
     return null;
