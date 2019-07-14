c74183067ce1bd85c08e0b7aaca5213af1eeabf6
Steve Min <stevekmin@gmail.com>
2019-07-14 00:14:57 +0000

    Define a stateless CartSummaryItem component that takes a cart item via props and displays the item's image, name, price, and short description.

---
+++ client/components/cart-summary-item.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+export default function ProductList(props) {
+  return (
+    <div className='col'>
+      <div className='row'>
+        <div>Item Image</div>
+        <div>Item Name</div>
+        <div>Item Price</div>
+        <div>Short Description</div>
+      </div>
+    </div>
+  );
+}
