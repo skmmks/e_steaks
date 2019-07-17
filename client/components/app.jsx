9598b546f0380397c74869352d7875a6b3f04058
Steve Min <stevekmin@gmail.com>
2019-07-17 01:48:09 +0000

    add initial cart state to App comp create checkoutform component set initial state for checkout form

--- client/components/app.jsx
+++ client/components/app.jsx
@@ -43,7 +43,8 @@ export default class App extends React.Component {
         view: {
           name: 'catalog',
           params: {}
-        }
+        },
+        cart: []
       }))
       .catch(error => error);
   }
