b074087eb63ae8d077b5f0eb63aca8d91855d3e3
Steve Min <stevekmin@gmail.com>
2019-07-18 04:00:17 +0000

    save point

--- server/public/api/products.php
+++ server/public/api/products.php
@@ -10,25 +10,36 @@ require_once('db_connection.php');
 startup();
 
 if(empty($_GET['id'])) {
-    $query = "SELECT * FROM `products`";
+    $query = "SELECT p.id, p.name, p.price, p.shortDescription, 
+        (SELECT `url` FROM `images` WHERE `productID` = p.id LIMIT 1) AS `image`
+        FROM `products` AS p";
     $result = mysqli_query($conn, $query);
     if(!$result) {
         throw new Exception('error with query: ' . mysqli_connect_error($conn));
-    }
-    $data = [];
-    while($row = mysqli_fetch_assoc($result)) {
-        $data[] = $row;
     }
     print(json_encode($data));
     } else if (!is_numeric($_GET['id'])) {
         throw new Exception('ID needs to be a number');
     } else {
     $id = $_GET['id'];
-    $query = "SELECT * FROM `products` WHERE `id` = " . $id;
+    $query = "SELECT p.id, p.name, p.price, p.shortDescription, 
+    GROUP_CONCAT(images.url) as images
+    FROM `images` AS i
+    JOIN `products` AS p
+    ON i.productID = p.id
+    WHERE p.`id` = " . $id .
+    " GROUP BY products.id
+    ";
     $result = mysqli_query($conn, $query);
-    $resultRow = mysqli_fetch_assoc($result);
+//    $resultRow = mysqli_fetch_assoc($result);
 
-    if ($resultRow === null) {
+    $data = [];
+    
+    while($resultRow = mysqli_fetch_assoc($result)) {
+        $data[] = $resultRow;
+    }
+
+    if ($resultRow === null && !empty($_GET['id'])) {
         throw new Exception('invalid ID: '. $id);
     } else {
         print(json_encode($resultRow));
