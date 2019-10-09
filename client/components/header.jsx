eebb8bd3a07f3551b7e8aaa5e9b19c06892f5229
Steve Min <stevekmin@gmail.com>
2019-10-09 04:57:08 +0000

    add shop icon to header component

--- client/components/header.jsx
+++ client/components/header.jsx
@@ -1,7 +1,11 @@
 import React from 'react';
 
 function Header() {
-  return <h1>ESteaks</h1>;
+  return (
+    <div>
+      <h1>ESteaks<i className="fas fa-store"></i></h1>
+    </div>
+  ) ;
 }
 
 export default Header;
