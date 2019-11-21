8ac4e4e3f1f1cffa21dd260cb316a7f007481508
Steve Min <stevekmin@gmail.com>
2019-11-21 19:38:57 +0000

    change store name to heritage farm add button to continue to catalog

--- client/components/landing-page.jsx
+++ client/components/landing-page.jsx
@@ -4,7 +4,10 @@ export default class LandingPage extends React.Component {
   render() {
     return (
       <div>
-        <h1>Hello World</h1>
+        <h1>Welcome to Heritage Farm Steaks</h1>
+        <div onClick={this.handleContinueButton} className={'btn btn-primary'}>
+            Continue to Catalog
+        </div>
       </div>
     );
   }
