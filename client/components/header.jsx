edab3b99b690198e49d10b717ecd931ab4b85350
Steve Min <stevekmin@gmail.com>
2019-11-21 17:37:03 +0000

    update company name

--- client/components/header.jsx
+++ client/components/header.jsx
@@ -14,11 +14,11 @@ export default class Header extends React.Component {
       <div className='col'>
         <div className="row">
           <div className="col text-right text-white bg-dark">
-            <h5>Order Before December 1st To Recieve %20 Off!</h5>
+            <h5>Order Before December 1st To Recieve 20% Off!</h5>
           </div>
         </div>
         <div className="row">
-          <h2 className='col text-center'> <i className="fas fa-store-alt"></i> The Steak Shop</h2>
+          <h2 className='col text-center'> <i className="fas fa-store-alt"></i> Hartland Farms</h2>
 
         </div>
         <div className="row">
