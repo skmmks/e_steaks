4e9888595d0309297a8fc2751d8295cdbba4bdc4
Steve Min <stevekmin@gmail.com>
2019-12-03 05:55:20 +0000

    add navbarbrand to navbar that takes user back to landingpage

--- client/components/navigation.jsx
+++ client/components/navigation.jsx
@@ -11,12 +11,17 @@ import {
 export default class Navigation extends React.Component {
     constructor(props) {
         super(props); 
+        this.handleLandingPage = this.handleLandingPage.bind(this); 
+    }
+    handleLandingPage(e) {
+        e.preventDefault(); 
+        this.props.setView('landingPage', {}); 
     }
     render() {
         return (
             <div>
                 <Navbar>
-
+                    <NavbarBrand onClick={this.handleLandingPage}>Heritage Farms</NavbarBrand>
                 </Navbar>
             </div>
         )
