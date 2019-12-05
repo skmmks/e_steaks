4deb77f3ffdbaf66fd61fc7e7fc2b3f69fcf7e0e
Steve Min <stevekmin@gmail.com>
2019-12-05 03:46:17 +0000

    refactor toggle function add click handler so that toggle fires

--- client/components/navigation.jsx
+++ client/components/navigation.jsx
@@ -12,19 +12,23 @@ export default class Navigation extends React.Component {
     constructor(props) {
         super(props); 
         this.handleLandingPage = this.handleLandingPage.bind(this); 
+        this.toggle = this.toggle.bind(this);
     }
     handleLandingPage(e) {
         e.preventDefault(); 
         this.props.setView('landingPage', {}); 
     }
-    toggle = () => {
-        setIsOpen(!isOpen)
+    toggle() {
+        this.setState({
+            isOpen: !this.state.isOpen
+        });
     }
     render() {
         return (
             <div>
                 <Navbar>
                     <NavbarBrand onClick={this.handleLandingPage}>Heritage Farms</NavbarBrand>
+                    <NavbarToggler onClick={this.toggle} /> 
                 </Navbar>
             </div>
         )
