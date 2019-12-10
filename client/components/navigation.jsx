5bb4ec1f42e9e0727a6c81d5bf7202a3f33d2ca9
Steve Min <stevekmin@gmail.com>
2019-12-10 18:21:44 +0000

    add change view to cart to navbar using reactstrap

--- client/components/navigation.jsx
+++ client/components/navigation.jsx
@@ -15,11 +15,17 @@ export default class Navigation extends React.Component {
             isOpen: false
         }
         this.handleLandingPage = this.handleLandingPage.bind(this); 
+        this.handleDisplayCart = this.handleDisplayCart.bind(this); 
         this.toggle = this.toggle.bind(this);
     }
     handleLandingPage(e) {
         e.preventDefault(); 
         this.props.setView('landingPage', {}); 
+    }
+    handleDisplayCart(e) {
+        e.preventDefault(); 
+        this.props.setView('cart', {});
+        console.log('cart was clicked')
     }
     toggle() {
         this.setState({
@@ -32,6 +38,8 @@ export default class Navigation extends React.Component {
                 <Navbar>
                     <NavbarBrand onClick={this.handleLandingPage}>Heritage Farms</NavbarBrand>
                     <NavbarToggler onClick={this.toggle} /> 
+                    <NavItem onClick={this.handleDisplayCart}>cart</NavItem> 
+                    <NavItem>about</NavItem> 
                 </Navbar>
             </div>
         )
