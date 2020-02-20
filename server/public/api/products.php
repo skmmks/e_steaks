f50cff3c54ca260321b4440f84a770d235a099cc
Steve Min <stevekmin@gmail.com>
2020-02-20 21:06:16 +0000

    refactor products.php

--- server/public/api/products.php
+++ server/public/api/products.php
@@ -1,37 +1,42 @@
 <?php
 
-header('Content-Type: application/json');
-require('functions.php');
-
-set_exception_handler('error_handler');
-
-require_once('db_connection.php');
+require_once 'functions.php';
+require_once 'db_connection.php';
 
 startup();
 
-if(empty($_GET['id'])) {
-    $query = "SELECT * FROM `products`";
-    $result = mysqli_query($conn, $query);
-    if(!$result) {
-        throw new Exception('error with query: ' . mysqli_connect_error($conn));
-    }
-    $data = [];
-    while($row = mysqli_fetch_assoc($result)) {
-        $data[] = $row;
-    }
-    print(json_encode($data));
-    } else if (!is_numeric($_GET['id'])) {
-        throw new Exception('ID needs to be a number');
-    } else {
-    $id = $_GET['id'];
-    $query = "SELECT * FROM `products` WHERE `id` = " . $id;
-    $result = mysqli_query($conn, $query);
-    $resultRow = mysqli_fetch_assoc($result);
-
-    if ($resultRow === null) {
-        throw new Exception('invalid ID: '. $id);
-    } else {
-        print(json_encode($resultRow));
-    }
+if(!$conn) {
+  throw new Exception(mysqli_connect_error());
 }
+
+$whereClause = ''; 
+$id = false;
+if(!empty($_GET['id'])) { 
+  if(!is_numeric($_GET['id'])) { 
+    throw new Exception('id must be an int');
+  }
+  $id = (int)$_GET['id']; 
+  $whereClause = " WHERE id = $id";
+}
+
+$query = "SELECT * FROM `products` $whereClause"; 
+$result = mysqli_query($conn, $query);
+
+if(!$result) {
+  throw new Exception(mysqli_error($conn));
+}
+
+if(mysqli_num_rows($result) === 0 && $id !== false) { 
+  throw new Exception("Invalid id: {$id}");
+}
+
+$output = [];
+while($row = mysqli_fetch_assoc($result)) { 
+  $row['price'] = intval($row['price']); 
+  $row['id'] = (int)$row['id']; 
+  $output[] = $row; 
+}
+
+print_r(json_encode($output));
+
 ?>
