5f5c4fa1700ecfcae602d94df740660a42a7625d
Steve Min <stevekmin@gmail.com>
2020-02-20 20:57:26 +0000

    modify readfile of cart php file

--- server/public/api/cart.php
+++ server/public/api/cart.php
@@ -6,7 +6,7 @@ $method = $_SERVER['REQUEST_METHOD'];
 $item = file_get_contents('php://input');
 
 if ($method == 'GET') {
-  readfile('dummy-cart-items.json');
+  readfile('dummy-cart.json');
 } else if ($method == 'POST') {
   http_response_code(201);
   print($item);
@@ -14,7 +14,7 @@ if ($method == 'GET') {
   http_response_code(404);
   print(json_encode([
     'error' => 'Not Found',
-    'message' => "Cannot $method /api/cart.php Some text" 
+    'message' => "Cannot $method /api/cart.php"
   ]));
 }
 
