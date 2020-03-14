22b6b982fb78095dee9cd02628f9c277de14235e
Steve Min <stevekmin@gmail.com>
2020-03-14 13:34:55 -0700

    remove features dir refactor header

--- client/components/header.jsx
+++ client/components/header.jsx
@@ -4,11 +4,35 @@ import { Collapse, Nav, Navbar, NavbarBrand, NavbarToggler, NavItem, NavLink } f
 export default class Header extends React.Component {
   constructor(props) {
     super(props);
-    this.handleSetView = this.handleSetView.bind(this);
+    this.toggle = this.toggle.bind(this);
+    this.state = {
+      isOpen: false
+    };
+    this.handleLandingPageView = this.handleLandingPageView.bind(this);
+    this.handleCartView = this.handleCartView.bind(this);
+    this.handleCatalogView = this.handleCatalogView.bind(this);
+    this.handleAboutAppView = this.handleAboutAppView.bind(this);
   }
-  handleSetView(e) {
+  handleLandingPageView(e) {
+    e.preventDefault();
+    this.props.setView('landingPage', {});
+  }
+  handleCartView(e) {
     e.preventDefault();
     this.props.setView('cart', {});
+  }
+  handleCatalogView(e) {
+    e.preventDefault();
+    this.props.setView('catalog', {});
+  }
+  handleAboutAppView(e) {
+    e.preventDefault();
+    this.props.setView('aboutApp', {});
+  }
+  toggle() {
+    this.setState({
+      isOpen: !this.state.isOpen
+    });
   }
   render() {
     let orders = this.props.cartItemCount.reduce((total, product) => {
@@ -16,18 +40,45 @@ export default class Header extends React.Component {
       return total;
     }, 0);
     return (
-      <div className='col'>
-        <div className='row'>
-          <div className='col text-center text-white bg-dark'>
-            <h5>FREE SHIPPING ON ORDERS $49 OR MORE</h5>
-          </div>
-        </div>
-        <div className='row'>
-          <div className='col text-right headerBar'>
-            Login | Create Account <i className='fas fa-shopping-cart' onClick={this.handleSetView}></i> ({orders})
-          </div>
-        </div>
+      <div>
+        <Navbar color='dark' expand='md'>
+          <NavbarBrand className='header-font pointer-hover noselect' onClick={this.handleLandingPageView}>
+            Heritage Farm
+          </NavbarBrand>
+          <NavbarToggler onClick={this.toggle} />
+          <Collapse isOpen={this.state.isOpen} navbar>
+            <Nav className='ml-auto' navbar>
+              <NavLink>
+                <i className='fas fa-tractor fa-lg pointer-hover' onClick={this.handleLandingPageView}></i>
+              </NavLink>
+              <NavLink>
+                <i className='fas fa-utensils fa-lg pointer-hover' onClick={this.handleCatalogView}></i>
+              </NavLink>
+              <NavLink>
+                <i className='fas fa-shopping-cart fa-lg pointer-hover' onClick={this.handleCartView}>
+                  {orders}
+                </i>
+              </NavLink>
+              <NavLink>
+                <i className='fas fa-question-circle fa-lg pointer-hover' onClick={this.handleAboutAppView}></i>
+              </NavLink>
+            </Nav>
+          </Collapse>
+        </Navbar>
       </div>
     );
   }
 }
+
+// <div className='col'>
+//   <div className='row'>
+//     <div className='col text-center text-white bg-dark'>
+//       <h5>FREE SHIPPING ON ORDERS $49 OR MORE</h5>
+//     </div>
+//   </div>
+//   <div className='row'>
+//     <div className='col text-right headerBar'>
+//       Login | Create Account <i className='fas fa-shopping-cart' onClick={this.handleSetView}></i> ({orders})
+//     </div>
+//   </div>
+// </div>
