0dfb0611b7fdc8bef90c80b99b2efd6962e0ec52
Steve Min <stevekmin@gmail.com>
2020-03-31 11:41:04 -0700

    update styling for catalog page

--- client/components/product-list.jsx
+++ client/components/product-list.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import ProductListItem from './product-list-item';
+import { Container, Row } from 'reactstrap';
 
 export default class ProductList extends React.Component {
   render() {
@@ -7,11 +8,36 @@ export default class ProductList extends React.Component {
       <ProductListItem key={merch.id} merch={merch} setView={this.props.setView} />
     ));
     return (
-      <div className='container'>
-        <h1 className='catalogHeader'>Inventory</h1>
+      // <div className='container'>
+      //   <div>
+      //     <img className='catalogBanner' src='/images/catalog-banner.jpg' alt='' />
+      //   </div>
+      //   <h1 className='catalogHeader'>classic lineup</h1>
+      //   <hr />
+      //   <div className='row'>{merch}</div>
+      // </div>
+      <React.Fragment>
+        <div>
+          <img className='catalogBanner' src='./images/catalog-banner.jpg' alt='Catalog banner image' />
+        </div>
         <hr />
-        <div className='row'>{merch}</div>
-      </div>
+        <div>
+          <h1 className='text-center'>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugit ad unde nihil facilis cum odio, molestiae
+            nesciunt? Ab eveniet nesciunt, cupiditate molestias quos tempore consequuntur reprehenderit quasi. Omnis,
+            corporis et.
+          </h1>
+        </div>
+        <hr />
+        <Container>
+          <Row>{merch}</Row>
+        </Container>
+      </React.Fragment>
     );
   }
 }
+
+// <React.Fragment>
+//   <img className='banner-image' src='./media/catalog-hero.jpg' alt='Catalog banner image' />
+//   <Row className='justify-content-md-center mr-1 ml-1'>{items}</Row>
+// </React.Fragment>;
